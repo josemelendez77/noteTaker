9646858baca3d2871430f45723ad99debe8fa082
josemelendez77 <141665270+josemelendez77@users.noreply.github.com>
2023-12-12 01:04:16 -0700

    final comit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,56 +1,39 @@
-// DEPENDENCIES
-// Routing
+
 const express = require("express");
 const apiRouter = express.Router();
-// file-system
 const fs = require("fs");
 
-//GET
-// Read the `db.json` file and return all saved notes as JSON.
 apiRouter.get("/api/notes", (req, res) => {
 
     fs.readFile('./db/db.json', 'utf8', (error, file) => {
         if (error) throw error;
 
-        // return parsed note text
+    
         const parsedFile = JSON.parse(file);
         return res.send(parsedFile);
     });
 });
 
-//POST
-// receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 apiRouter.post("/api/notes", (req, res) => {
-    // 1. read db file first and parse it! 
-    // 2. then, push new note to it
-    // 3. then write back the stringified version of it
-
-    // construct incoming note for JSON object
     let note = req.body;
     note["id"] = Date.now();
     note["title"] = req.body.title;
     note["text"] = req.body.text;
 
-    // reading from the file: have string to convert into an object - use parse
     fs.readFile('./db/db.json', 'utf8', (error, file) => {
         if (error) throw error;
 
-        // parse db.json into a JSON object
+        
         const parsedFile = JSON.parse(file);
-        // push the new note onto the JSON object
         parsedFile.push(note);
 
-        // Create new stringify of the combined file to write back to file
         const newStringifiedFile = JSON.stringify(parsedFile);
     
-        // re-write the file as the combined file
         fs.writeFile('./db/db.json', newStringifiedFile, 'utf8', (err) => {
             if (err) throw err;
             console.log("The new note was appended to the file!");
         });
         
-        // return parsed version of new stringified file to read back to the page
-        // adding this resolved ERR EMPTY RESPONSE
         return res.send(JSON.parse(newStringifiedFile));
     });    
 });
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-// DEPENDENCIES
-// Set-up express server & tell node we are creating an express server
+
 const express = require("express");
 const app = express();
 
-// Sets an initial port for listener
 let PORT = process.env.PORT || 4000;
 
 // Routers
@@ -11,19 +9,12 @@ const apiRouter = require("./routes/apiRoutes");
 const htmlRouter = require("./routes/htmlRoutes");
 
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Middleware - node girls step 6 - https://node-girls.gitbook.io/intro-to-express/
 app.use(express.static("public"));
 app.use(express.static("db"));
-// order matters here!
 app.use(apiRouter);
 app.use(htmlRouter);
 
-// Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-//run 'npm start' or 'nodemon server.js' to start the server
-//https://medium.com/@ralph1786/how-to-setup-an-express-server-5fd9cd9ae073
